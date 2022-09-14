59665fe5eae2cf1454e13dc51fcac764d45870df
RRicsi89 <rozsar1989@gmail.com>
2022-09-14 13:40:40 +0200

    implement Servlet to delete cancelled Orders

---
+++ src/main/java/com/codecool/shop/controller/DeleteOrderServlet.java
@@ -0,0 +1,24 @@
+package com.codecool.shop.controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = {"/order/delete"})
+public class DeleteOrderServlet extends HttpServlet {
+
+    @Override
+    public void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.addHeader("Access-Control-Allow-Origin", "*");
+        String orderID = request.getParameter("orderId");
+
+        // TODO implement OrderDaoMem delete method
+
+        PrintWriter out = response.getWriter();
+        out.flush();
+    }
+}
