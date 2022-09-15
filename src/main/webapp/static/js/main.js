be06b4192be72cd611b3550dde5e399fba3017b5
Suto Gabor <wakacorpz@gmail.com>
2022-09-15 11:24:33 +0200

    add elementCreator function to main.js

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -1,3 +1,10 @@
+function elementCreator(type, className) {
+    const element = document.createElement(type);
+    element.classList.add(className);
+    return element;
+}
+
+
 async function apiGet(url) {
     let response = await fetch(url, {method: "GET"});
     if(response.status === 200) {
