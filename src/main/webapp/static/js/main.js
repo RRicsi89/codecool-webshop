405dbd4af3dbd8644c8b59ebe844fee1c7e0ad4c
Suto Gabor <wakacorpz@gmail.com>
2022-09-15 11:25:21 +0200

    add cardBuilder function to main.js

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -1,3 +1,43 @@
+function cardBuilder(products) {
+    const productsDiv = document.getElementById("products");
+    productsDiv.innerHTML = "";
+    for(let product of products) {
+        const card = elementCreator("div", "card");
+        const image = elementCreator("img", "image");
+        const cardHeader = elementCreator("div", "card-header");
+        const cardTitle = elementCreator("h4", "card-title");
+        const cardHeaderText = elementCreator("p", "lead");
+        const cardBody = elementCreator("div", "card-body");
+        const cardBodyTextCont = elementCreator("div", "card-text");
+        const cardBodyText = elementCreator("p", "lead");
+        const cartBtnContainer = elementCreator("div", "card-text");
+        const cartButton = elementCreator("a", "btn-success");
+        cartButton.classList.add("btn");
+        cardHeaderText.classList.add("price");
+        cardHeaderText.classList.add("desc");
+
+        image.src = "/static/img/product_1.jpg";
+        cardTitle.textContent = product.name;
+        cardHeaderText.textContent = product.description;
+        cardBodyText.textContent = product.defaultPrice;
+        cartButton.href = "#";
+        cartButton.textContent = "Add to cart";
+
+        card.appendChild(image);
+        card.appendChild(cardHeader);
+        card.appendChild(cardBody);
+        cardHeader.appendChild(cardTitle);
+        cardHeader.appendChild(cardHeaderText);
+        cardBody.appendChild(cardBodyTextCont);
+        cardBody.appendChild(cartBtnContainer);
+        cardBody.appendChild(cartBtnContainer);
+        cartBtnContainer.appendChild(cartButton);
+        cardBodyTextCont.appendChild(cardBodyText);
+        productsDiv.appendChild(card);
+    }
+}
+
+
 function elementCreator(type, className) {
     const element = document.createElement(type);
     element.classList.add(className);
