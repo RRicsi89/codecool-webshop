023a1ccd848c3bc56bd651777980e6194fe465cd
RRicsi89 <rozsar1989@gmail.com>
2022-09-28 19:58:26 +0200

    Refactor: alter database with additional columns

--- src/main/webapp/sample_data/create_db.sql
+++ src/main/webapp/sample_data/create_db.sql
@@ -1,8 +1,9 @@
 ALTER TABLE IF EXISTS ONLY public.categories DROP CONSTRAINT IF EXISTS fk_department_id CASCADE;
 ALTER TABLE IF EXISTS ONLY public.orders DROP CONSTRAINT IF EXISTS fk_user_id CASCADE;
 ALTER TABLE IF EXISTS ONLY public.orders DROP CONSTRAINT IF EXISTS fk_product_id CASCADE;
-ALTER TABLE IF EXISTS ONLY public.products DROP CONSTRAINT IF EXISTS fk_category_id CASCADE;
 ALTER TABLE IF EXISTS ONLY public.products DROP CONSTRAINT IF EXISTS fk_supplier_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.product_categories DROP CONSTRAINT IF EXISTS fk_category_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.product_categories DROP CONSTRAINT IF EXISTS fk_product_category_id CASCADE;
 
 DROP TABLE IF EXISTS public.users;
 CREATE TABLE public.users (
@@ -14,7 +15,8 @@ CREATE TABLE public.users (
 DROP TABLE IF EXISTS public.suppliers;
 CREATE TABLE public.suppliers (
     id SERIAL NOT NULL PRIMARY KEY,
-    name TEXT
+    name TEXT,
+    description TEXT
 );
 
 DROP TABLE IF EXISTS public.products;
@@ -23,8 +25,15 @@ CREATE TABLE public.products (
     name TEXT,
     price DOUBLE PRECISION,
     currency TEXT,
-    category_id INTEGER,
-    supplier_id INTEGER
+    supplier_id INTEGER,
+    description TEXT,
+    image TEXT
+);
+
+DROP TABLE IF EXISTS public.product_categories;
+CREATE TABLE public.product_categories (
+    product_id INTEGER,
+    category_id INTEGER
 );
 
 DROP TABLE IF EXISTS public.orders;
@@ -45,7 +54,8 @@ DROP TABLE IF EXISTS public.categories;
 CREATE TABLE public.categories (
     id SERIAL NOT NULL PRIMARY KEY,
     name TEXT,
-    department_id INTEGER
+    department_id INTEGER,
+    description TEXT
 );
 
 ALTER TABLE ONLY categories
@@ -55,6 +65,8 @@ ALTER TABLE ONLY orders
 ALTER TABLE ONLY orders
     ADD CONSTRAINT fk_product_id FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE;
 ALTER TABLE ONLY products
-    ADD CONSTRAINT fk_category_id FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE;
-ALTER TABLE ONLY products
     ADD CONSTRAINT fk_supplier_id FOREIGN KEY (supplier_id) REFERENCES suppliers(id) ON DELETE CASCADE;
+ALTER TABLE ONLY product_categories
+    ADD CONSTRAINT fk_category_id FOREIGN KEY (category_id) REFERENCES categories(id) ON DELETE CASCADE;
+ALTER TABLE ONLY product_categories
+    ADD CONSTRAINT fk_product_category_id FOREIGN KEY (product_id) REFERENCES products(id) ON DELETE CASCADE;
