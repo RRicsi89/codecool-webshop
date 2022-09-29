e91146f2f3109985cba20a5538498c618a1a0dbc
RRicsi89 <rozsar1989@gmail.com>
2022-09-29 14:49:47 +0200

    Add necessary data to product_categories table

--- src/main/webapp/sample_data/sample_data.sql
+++ src/main/webapp/sample_data/sample_data.sql
@@ -29,3 +29,14 @@ VALUES ('Something Stupid', 0.99, 'USD', 3, 'Stupid', '/static/img/product_1.jpg
        ('Something Lifesaving', 99.99, 'USD', 1, 'Saves life', '/static/img/product_7.jpg'),
        ('Something Sickening', 199.99, 'USD', 1, 'Sickens life', '/static/img/product_8.jpg'),
        ('Something Healthy', 999.99, 'USD', 1, 'Prolongs life', '/static/img/product_9.jpg');
+
+INSERT INTO public.product_categories
+VALUES (1, 1), (1, 5), (1, 4),
+        (2, 6), (2, 4), (2, 2),
+        (3, 6), (3, 3), (3, 1),
+        (4, 10), (4, 3), (4, 8),
+        (5, 3), (5, 7), (5, 5),
+        (6, 8), (6, 9), (6, 6), (6, 4),
+        (7, 6), (7, 4), (7, 9), (7, 2),
+        (8, 6), (8, 3), (8, 11), (8, 2),
+        (9, 6), (9, 3), (9, 8), (9, 2);
