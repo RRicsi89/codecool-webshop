e88a57a3fe2771b758df0b92f064e27bc2e248b2
RRicsi89 <rozsar1989@gmail.com>
2022-09-29 20:29:03 +0200

    Add js files to handle user login/register events

--- src/main/java/com/codecool/shop/controller/PaymentProcessServlet.java
+++ src/main/java/com/codecool/shop/controller/PaymentProcessServlet.java
@@ -1,7 +1,11 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.DatabaseManager;
+import com.codecool.shop.dao.UserDao;
+import com.codecool.shop.dao.implementation.UserDaoJdbc;
 import com.codecool.shop.log.Logger;
+import com.codecool.shop.model.Item;
 import com.codecool.shop.model.Order;
 import com.codecool.shop.service.PaymentValidator;
 import org.thymeleaf.TemplateEngine;
@@ -12,7 +16,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet(urlPatterns = {"/api/process-payment"})
 public class PaymentProcessServlet extends HttpServlet {
@@ -20,6 +26,8 @@ public class PaymentProcessServlet extends HttpServlet {
     @Override
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         response.addHeader("Access-Control-Allow-Origin", "*");
+        DataSource dataSource = DatabaseManager.connect();
+        UserDao userManager = UserDaoJdbc.getInstance(dataSource);
 
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
         WebContext context = new WebContext(request, response, request.getServletContext());
@@ -40,8 +48,14 @@ public class PaymentProcessServlet extends HttpServlet {
 
         if (paymentCheck) {
             context.setVariable("cart", order.getCart());
-            // TODO send email
+            int userId = ProductController.userId;
             Logger.logToFile(order, true);
+            List<Item> items = order.getCart();
+            for (Item item: items) {
+                int productId = item.getProduct().getId();
+                int quantity = item.getQuantity();
+                userManager.saveUserOrder(userId, productId, quantity);
+            }
             Order.deleteOrder();
             engine.process("product/confirmation.html", context, response.getWriter());
         } else {
---
+++ src/main/webapp/static/js/userLogin.js
@@ -0,0 +1,70 @@
+
+function createModal(message) {
+    const modal = document.createElement("div");
+    const modalBody = createModalBody();
+    const modalHeader = createModalHeader();
+    const modalFooter = createModalFooter(message);
+
+    const modalContainer = document.createElement("div");
+    modalContainer.classList.add("modal");
+
+    modalContainer.appendChild(modalHeader);
+    modalContainer.appendChild(modalBody);
+    modalContainer.appendChild(modalFooter);
+
+    return modalContainer;
+}
+
+function createModalFooter(message) {
+    const modalFooter = document.createElement("div");
+    modalFooter.classList.add("modal-footer");
+    const submitBtn = document.createElement("button");
+    submitBtn.classList.add("submit-btn");
+    submitBtn.innerText = message;
+    submitBtn.type = "submit";
+    const cancelBtn = document.createElement("button");
+    cancelBtn.classList.add("cancel-btn");
+    cancelBtn.innerText = "Cancel";
+    modalFooter.appendChild(submitBtn);
+    modalFooter.appendChild(cancelBtn);
+    submitBtn.setAttribute("form", "login-form");
+    return modalFooter;
+}
+
+function createModalHeader() {
+    const modalHeader = document.createElement("div");
+    modalHeader.innerText = "User information";
+    modalHeader.classList.add("modal-header");
+    return modalHeader;
+}
+
+function createModalBody() {
+    const modalBody = document.createElement("div");
+    modalBody.classList.add("modal-body");
+    modalBody.innerHTML =
+        "<form id='login-form' action='/login' method='POST'>" +
+        "<label for='username'>Name</label></br>" +
+        "<input type='text' name='name' id='username' required></br></br>" +
+        "<label for='password'>Password</label></br>" +
+        "<input type='password' name='password' id='password' required></br></br>" +
+        "</form>"
+    return modalBody;
+}
+
+function applyEventListenerToLoginBtn() {
+    const loginBtn = document.querySelector("#login-btn");
+    loginBtn.addEventListener("click", () => {
+        const modal = createModal("Login");
+        const layer = document.createElement("div");
+        layer.classList.add("layer");
+        document.body.appendChild(modal);
+        document.body.appendChild(layer);
+        const cancelBtn = document.querySelector(".cancel-btn");
+        cancelBtn.addEventListener("click", () => {
+            modal.remove();
+            layer.remove();
+        })
+    })
+}
+
+applyEventListenerToLoginBtn();
